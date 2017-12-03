e2a6b77e397c993b3ebce4331ce2a5e02ceb766a
Perry Pellerino <perrypel@gmail.com>
2017-12-03 08:35:33 -0500

    Added som files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component,
+    Page4Component
   ],
   imports: [
     BrowserModule
